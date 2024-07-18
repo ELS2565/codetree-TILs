0f9fd389b7300759b253aff023342ec06fc867d6
ELS2565 <152383431+ELS2565@users.noreply.github.com>
2024-07-18 21:03:57 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if (a == 5) { cout << "A" << '\n';}
+    else if (a % 2 == 0) { cout << "B" << '\n';}
+    return 0;
+}
